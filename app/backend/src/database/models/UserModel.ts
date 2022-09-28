48de00211dfbe751d52fb3ce1657c7a655365369
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-09-28 18:05:38 -0300

    feat: Req. 01 - criado model e migrations para tabela 'users' e renomeado seeders para essa tabela

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,53 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+export default class User extends Model {
+  public id: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;  
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+  // ... Outras configs
+  // underscored: true,
+  sequelize: db,
+  // modelName: 'users',
+  tableName: 'users',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS: 
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
