3a147854dbb2b654fc1daa53781626538964a87a
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-09-28 21:51:39 -0300

    feat: Req. 14 - criado model e migrations para tabela 'teams' e renomeado seeders para essa tabela

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Match from './MatchModel';
+// import OtherModel from './OtherModel';
+
+export default class Team extends Model {
+  public id: number;
+  public teamName: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  // modelName: 'users',
+  tableName: 'teams',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'mandante' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'visitante' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
