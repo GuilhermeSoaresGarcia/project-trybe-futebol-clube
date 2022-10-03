4c5f04d3aa7bb32c8a6262318a5adcf134e3f392
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-03 18:18:35 -0300

    feat: Implementa validação para não ser possível cadastrar o mesmo time como mandante e visitante

--- app/backend/src/controllers/MatchControllers.ts
+++ app/backend/src/controllers/MatchControllers.ts
@@ -13,6 +13,13 @@ export default class MatchController {
   }
 
   static async createNewMatch(data: INewMatch) {
+    if (data.awayTeam === data.homeTeam) {
+      return {
+        code: 401,
+        message: { message: 'It is not possible to create a match with two equal teams' },
+      };
+    }
+
     const result = await MatchServices.createNewMatch(data);
     return result;
   }
