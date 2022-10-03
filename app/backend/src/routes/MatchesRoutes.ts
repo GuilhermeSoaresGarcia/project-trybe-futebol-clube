924ebe4d96ca797a07df17001dab5517fb31019d
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-03 16:32:22 -0300

    feat: adicionada lógica na rota de matches para poder utilizar a query 'inProgress'

--- app/backend/src/routes/MatchesRoutes.ts
+++ app/backend/src/routes/MatchesRoutes.ts
@@ -3,9 +3,15 @@ import MatchController from '../controllers/MatchControllers';
 
 const routes = Router();
 
-routes.get('/matches', async (_req, res) => {
-  const { code, message } = await MatchController.getAllMatches();
-  res.status(code).json(message);
+routes.get('/matches', async (req, res) => {
+  const { inProgress } = req.query;
+  if (inProgress === undefined) {
+    const { code, message } = await MatchController.getAllMatches();
+    return res.status(code).json(message);
+  }
+  const { code, message } = await MatchController
+    .getMatchesInProgress(inProgress as string);
+  return res.status(code).json(message);
 });
 
 export default routes;
