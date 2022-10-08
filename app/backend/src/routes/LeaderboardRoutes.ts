832b3785db03cd92737ca8f6993e413905617b65
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-10-08 19:34:36 -0300

    feat: adicionada rota '/leaderboard/away/' com método GET para listar a tabela de classificação dos times visitantes

--- app/backend/src/routes/LeaderboardRoutes.ts
+++ app/backend/src/routes/LeaderboardRoutes.ts
@@ -8,4 +8,9 @@ routes.get('/leaderboard/home', async (_req, res) => {
   res.status(code).json(message);
 });
 
+routes.get('/leaderboard/away', async (_req, res) => {
+  const { code, message } = await LeaderboardController.getAllAway();
+  res.status(code).json(message);
+});
+
 export default routes;
